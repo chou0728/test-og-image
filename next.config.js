2ca9e375134111085e6e00295f12e1d26bcbb65d
chou0728 <dean34520@gmail.com>
2024-06-14 17:22:49 +0800

    change basePath

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@
 // static export docs
 // https://nextjs.org/docs/app/building-your-application/deploying/static-exports#configuration
 const nextConfig = {
-  basePath: '/github-pages',
+  basePath: '/test-og-image',
   output: 'export',
 };
 
