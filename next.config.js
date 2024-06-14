79b6b6f7aed0068cbcc6b5c2163e3ecc1a2c12d2
chou0728 <dean34520@gmail.com>
2024-06-14 17:15:49 +0800

    setup config

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@
 // static export docs
 // https://nextjs.org/docs/app/building-your-application/deploying/static-exports#configuration
 const nextConfig = {
+  basePath: '/test-og-image',
   output: 'export',
   distDir: 'dist',
 };
