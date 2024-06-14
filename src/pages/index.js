c0eb9a5e8b5a0ce28b5df7b38e3e19740a5d23d1
chou0728 <dean34520@gmail.com>
2024-06-14 17:27:17 +0800

    update image url

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ const Index = () => {
         openGraph={{
           images: [
             {
-              url: '/static/img/icons/logo.png',
+              url: '/logo.png',
               alt: 'logo',
             },
           ],
