6533ef3dfc7beac65d1e16ae0a082fce6bfb231d
chou0728 <dean34520@gmail.com>
2024-06-28 16:43:50 +0800

    update images

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,12 @@
 import { Box } from '@chakra-ui/react';
 import { NextSeo } from 'next-seo';
 import Image from 'next/image';
-import stepSvg from '/public/apply-for-fund-step-1.svg';
-import stepPng from '/public/apply-for-fund-step-1.png';
+import step1Svg from '/public/apply-for-fund-step-1.svg';
+import step1Png from '/public/apply-for-fund-step-1.png';
+import step2Svg from '/public/apply-for-fund-step-2.svg';
+import step2Png from '/public/apply-for-fund-step-2.png';
+import step3Svg from '/public/apply-for-fund-step-3.svg';
+import step3Png from '/public/apply-for-fund-step-3.png';
 
 const Index = () => {
   return (
@@ -22,8 +26,12 @@ const Index = () => {
         }}
       />
       <Box p="20px" color="red">
-        <Image src={stepSvg} width={260} height={258} alt="test" />
-        <Image src={stepPng} width={260} height={258} alt="test" />
+        <Image src={step1Svg} width={260} height={258} alt="test" />
+        <Image src={step1Png} width={260} height={258} alt="test" />
+        <Image src={step2Svg} width={260} height={258} alt="test" />
+        <Image src={step2Png} width={260} height={258} alt="test" />
+        <Image src={step2Svg} width={260} height={258} alt="test" />
+        <Image src={step2Png} width={260} height={258} alt="test" />
       </Box>
     </>
   );
